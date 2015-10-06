dc1df54bafda0a1cdd9caffc32068cf3bc0416f8
eaw5117 <eric.washington.me@gmail.com>
2015-10-06 19:29:20 +0000

    Added ability to search by Name/Initial and Phone Number

--- address_book.rb
+++ address_book.rb
@@ -7,6 +7,16 @@ class AddressBook
     @contacts = []
   end
   
+  def print_results(search, results)
+    puts search
+    results.each do |contact|
+      puts contact.to_s('full_name')
+      contact.print_phone_numbers
+      contact.print_addresses
+      puts "\n"
+    end
+  end
+  
   def find_by_name(name)
     results = []
     search = name.downcase
@@ -15,13 +25,20 @@ class AddressBook
         results.push(contact)
       end
     end
-    puts "Name search results (#{search})"
-    results.each do |contact|
-      puts contact.to_s('full_name')
-      contact.print_phone_numbers
-      contact.print_addresses
-      puts "\n"
+    print_results("Name search results (#{search})", results)
+  end
+  
+  def find_by_phone_number(number)
+    results = []
+    search = number.gsub("-", "")
+    contacts.each do |contact|
+      contact.phone_numbers.each do |phone_number|
+        if phone_number.number.gsub("-", "").include?(search)
+          results.push(contact) unless results.include?(contact)
+        end
+      end
     end
+    print_results("Phone search results (#{search})", results)
   end
   
   def print_contact_list
@@ -31,6 +48,8 @@ class AddressBook
     end
   end
 end
+
+address_book = AddressBook.new
 
 address_book = AddressBook.new
 
@@ -47,3 +66,4 @@ address_book.contacts.push(eric)
 #address_book.print_contact_list
 
 address_book.find_by_name("e")
+address_book.find_by_phone_number("123")  
